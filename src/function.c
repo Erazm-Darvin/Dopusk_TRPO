24ed34c85a20cd6c5ce8e8f5ece58165150133e2
erazm <Erazm-Darvin>
2017-06-30 22:55:59 +0700

    123456

--- src/function.c
+++ src/function.c
@@ -33,10 +33,7 @@ int Proverka(float a, float b, float c)
 	   
     if (a == 0 && b!=0 ) {return 3;}
 	if (b == 0 && a!=0 ) {if ((c>0 && a>0) || (c<0 && a<0)) return 0; return 4;}
-    printf("1");
 	if ((a == 0) && (b == 0)){return 0;}
-
-	    printf("2");
         float D = Discriment(a,b,c);
 		if (D == 0){return 1;}
 		if (D < 0) {return 0;}
